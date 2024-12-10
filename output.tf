d3713dbf298352525d671f878a006f6a6aaa6599
Robert de Bock <robert.debock@adfinis.com>
2024-12-10 12:51:32 +0100

    Styleguide.

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "hcloud_server_ipv4_address" {
-  value = hcloud_server.default.ipv4_address
+  description = "The public IPv4 address of the server."
+  value       = hcloud_server.default.ipv4_address
 }
 
 output "hcloud_server_name" {
-  value = hcloud_server.default.name
+  description = "The name of the created server."
+  value       = hcloud_server.default.name
 }
